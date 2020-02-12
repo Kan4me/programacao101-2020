2ca567cae8f15b0207e06901b2932521b377f556
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-11 23:36:49 -0300

    Add files via upload

---
+++ Lista1-1.1.c
@@ -0,0 +1,12 @@
+/*Amanda Lara - Exercícios Lista Dia 1
+11/02/2020
+*/
+
+// 1.1 - Identação
+#include <stdio.h>
+int main(){
+    int saldo = 500;
+    printf("Saldo em Conta: %d", saldo);
+
+return 0;
+}
---
+++ Lista1-1.2.c
@@ -0,0 +1,23 @@
+/*Amanda Lara - Exercícios Lista Dia 1
+11/02/2020
+*/
+
+//Correção do código
+
+//1.2 a) a d)
+#include <stdio.h>
+
+int main(){
+    int deposito;
+    double divisao = 5 / 3;
+    printf("Hello World! \n");
+    printf("Estou aprendendo! \n");
+    printf("Esta facil! \n");
+    printf("Digite o valor do deposito: \n");
+    scanf( "%d", &deposito);
+    printf("%d \n", deposito);
+    printf("Resultado = %.5lf", divisao);
+
+return 0;
+}
+
