1a60a3dd72d8f75c034e48ec37d9732510e4d805
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-13 15:17:35 -0300

    Update Lista1-1.2.c

--- exercicios1/Lista1-1.2.c
+++ exercicios1/Lista1-1.2.c
@@ -9,15 +9,14 @@
 
 int main(){
     int deposito;
-    double divisao = 5 / 3;
+    double divisao = 5.0 / 3.0;
     printf("Hello World! \n");
     printf("Estou aprendendo! \n");
     printf("Esta facil! \n");
-    printf("Digite o valor do deposito: \n");
+    printf("Digite o valor do deposito: ");
     scanf( "%d", &deposito);
-    printf("%d \n", deposito);
+    printf("O deposito realizado foi de %d \n", deposito);
     printf("Resultado = %.5lf", divisao);
 
 return 0;
 }
-
