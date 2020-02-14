70beba0032a022ca7f896e4dcf811bb504c9fb9f
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-14 15:58:38 -0300

    Add files via upload

---
+++ lista3-1.c
@@ -0,0 +1,25 @@
+/*Amanda Lara - Exercícios Lista Dia 3
+14/02/2020
+*/
+
+//Ecercicio 1001 URI
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int A, B, X;
+
+    printf("A: \n");
+    scanf( "%d", &A);
+    
+    printf("B: \n");
+    scanf( "%d", &B);
+
+    X = A + B;
+	
+	printf("X = %d \n", X);
+
+
+return 0;
+}
