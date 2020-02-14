cb5ac8a0a2e1c9bef5b1e5dc0eb9dab772ab1d5e
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-14 15:12:06 -0300

    Update Lista1-7.c

--- exercicios1/Lista1-7.c
+++ exercicios1/Lista1-7.c
@@ -1,4 +1,4 @@
-/*Amanda Lara - Exercícios Lista Dia 1
+/*Amanda Lara - ExercÃ­cios Lista Dia 1
 13/02/2020
 */
 
@@ -12,24 +12,24 @@ int main(){
     float distancia_viagem, litros_viagem, numero_tanques;
 
     printf("Digite o tempo da viagem em horas (valor inteiro): \n");
-    scanf( "%f", &horas_viagem);
+    scanf( "%d", &horas_viagem);
 
     printf("Digite a velocidade media durante a viagem (km/h): \n");
-    scanf( "%f", &velocidade_media);
+    scanf( "%d", &velocidade_media);
 
     printf("Digite a autonomia do veiculo (km/L): \n");
-    scanf( "%f", &autonomia);
+    scanf( "%d", &autonomia);
 
     printf("Digite a capacidade do tanque em litros: \n");
-    scanf( "%f", &tanque);
+    scanf( "%d", &tanque);
 
     distancia_viagem = horas_viagem * velocidade_media;
     litros_viagem = distancia_viagem / autonomia;
-    numero_tanques = round(litros_viagem / tanque);
+    numero_tanques = ceil(litros_viagem / tanque);
 
     printf("Uma viagem de %d horas tem a distancia de %.2f quilometros a uma velocidade media de %d \n",
            horas_viagem, distancia_viagem, velocidade_media);
-    printf("Essa viagem gasta %.2f litros e com um tanque de %d litros precisa abastecer %f vezes \n",
+    printf("Essa viagem gasta %.2f litros e com um tanque de %d litros precisa abastecer %.2f vezes \n",
            litros_viagem, tanque, numero_tanques);
 
 return 0;
