231d95fa7862958a45ee3c461d9e9e6fcfd3f028
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-17 17:34:32 -0300

    Create Lista5-1.c

---
+++ exercicios5/Lista5-1.c
@@ -0,0 +1,22 @@
+/*Amanda Lara - Exercícios Lista Dia 4
+17/02/2020
+*/
+
+//Lista string Aula 4 - Leitura de string
+//string eh um vetor de char! Espaco eh considerado uma posicao no vetor!
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main(){
+  char palavra[20];
+
+  printf("Digite uma palavra: \n");
+  scanf("%s", palavra);
+  printf("%s", palavra);
+
+return 0;
+}
