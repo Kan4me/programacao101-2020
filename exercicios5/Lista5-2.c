f9669017f75d86b57bd0fb47722655262e275835
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-17 17:36:54 -0300

    Create Lista5-2.c

---
+++ exercicios5/Lista5-2.c
@@ -0,0 +1,28 @@
+/*Amanda Lara - Exercícios Lista Dia 4
+17/02/2020
+*/
+
+//Lista string Aula 4 - Leitura de  2 string
+//string eh um vetor de char!
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main(){
+  char palavra1[20];
+  char palavra2[20];
+
+  printf("Digite palavra 1: \n");
+  scanf("%s", palavra1);
+
+  printf("Digite palavra 2: \n");
+  scanf("%s", palavra2);
+
+  printf("%s \n", palavra1);
+  printf("%s", palavra2);
+
+return 0;
+}
