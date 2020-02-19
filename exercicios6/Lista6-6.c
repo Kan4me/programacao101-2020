8515be4ad6e3c09edcef949d4a755d0316b6c878
Kan4me <57239338+Kan4me@users.noreply.github.com>
2020-02-19 17:09:07 -0300

    Create Lista6-6.c

---
+++ exercicios6/Lista6-6.c
@@ -0,0 +1,35 @@
+/*Amanda Lara - Exercícios Lista Dia 5
+19/02/2020
+*/
+
+//Lista string Aula 5 - Manipulacao de matrizes
+//Matriz Identidade
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int line, column, i, j;
+
+  printf("Digite o numero de linhas da matriz: \n");
+  scanf("%d", &line);
+
+  printf("Digite o numero de colunas da matriz: \n");
+  scanf("%d", &column);
+
+  int matrix[line][column];
+
+  for (i = 0; i <= line - 1; i ++){
+      for (j = 0; j <= column - 1; j ++){
+          if (i == j){
+            matrix [i][j] = 1;
+          } else {
+            matrix [i][j] = 0;
+          }
+          printf("%d ", matrix[i][j]);
+      } 
+      printf("\n");
+  }
+
+  return (0);
+}
